fc4676e6020475808df9510dca89166fcca4ac44
Rajawali Ramadhan Babelian <babelianrr@gmail.com>
2024-06-18 23:51:32 +0700

    finalization

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React, { useState, useEffect } from 'react'
 import detectEthereumProvider from "@metamask/detect-provider"
 import axios from 'axios'
+import Swal from 'sweetalert2'
 import './App.css'
 import './assets/css/sb-admin-2.min.css'
 import './assets/vendor/fontawesome-free/css/all.min.css'
+import bitcoin from './assets/img/bitcoin.png'
+import bnb from './assets/img/bnb.png'
+import ethereum from './assets/img/ethereum.png'
+import solana from './assets/img/solana.png'
+import tether from './assets/img/tether.png'
 
 function App() {
     const [currentAccount, setCurrentAccount] = useState("")
     const [currentBalance, setCurrentBalance] = useState("")
     const [currentListings, setCurrentListings] = useState([])
     const URL = 'pro-api.coinmarketcap.com'
+    const images = [bitcoin, ethereum, tether, bnb, solana]
+
+    const parseNumber = (number) => {
+        return Intl.NumberFormat("en-US", {
+            maximumFractionDigits: 2,
+        }).format(number)
+    }
 
     const setup = async () => {
         const provider = await detectEthereumProvider()
@@ -60,44 +73,68 @@ function App() {
                 account,
                 "latest"
             ]
-        });
+        })
         setCurrentBalance(parseInt(balance, 16).toString())
     }
 
     const getCrypto = async () => {
-        let response = null;
+        let response = null
+        Swal.fire({
+            title: "Loading...",
+            timer: 2000,
+            didOpen: () => {
+                Swal.showLoading()
+            }
+        })
         new Promise(async (resolve, reject) => {
             try {
                 response = await axios.get(`https://${URL}/v1/cryptocurrency/listings/latest`, {
                     headers: {
                         'X-CMC_PRO_API_KEY': '748d58ed-7963-48fb-b28b-1f2b5f189885',
                     },
-                });
+                    params: {
+                        limit: 5,
+                    }
+                })
             } catch (ex) {
-                response = null;
+                response = null
                 // error
-                console.log(ex);
-                reject(ex);
+                console.log(ex)
+                reject(ex)
             }
             if (response) {
                 // success
-                const json = response.data;
-                setCurrentListings(response.data.data.slice(0, 5))
-                resolve(json);
+                const json = response.data
+                setCurrentListings(response.data.data)
+                resolve(json)
             }
-        });
+        })
     }
 
-    const removeAccount = async () => {
-        await window.ethereum.request({
-            "method": "wallet_revokePermissions",
-            "params": [
-                {
-                    "eth_accounts": {}
-                }
-            ]
-        });
-        localStorage.removeItem('account')
+    const removeAccount = () => {
+        Swal.fire({
+            text: 'Are you sure to disconnect wallet?',
+            icon: 'question',
+            showConfirmButton: true,
+            showCancelButton: true,
+            confirmButtonText: 'Yes',
+            cancelButtonText: 'No'
+        }).then(async (result) => {
+            if (result.isConfirmed) {
+                await window.ethereum.request({
+                    "method": "wallet_revokePermissions",
+                    "params": [
+                        {
+                            "eth_accounts": {}
+                        }
+                    ]
+                })
+                localStorage.removeItem('account')
+                setCurrentAccount("")
+                setCurrentBalance("")
+                window.location.reload()
+            }
+        })
     }
 
     useEffect(() => {
@@ -119,8 +156,8 @@ function App() {
 
                     <li className="nav-item active">
                         <a className="nav-link" href="index.html">
-                            <i className="fas fa-fw fa-tachometer-alt"></i>
-                            <span>Dashboard</span>
+                            <i className="fas fa-fw fa-home"></i>
+                            <span>Home</span>
                         </a>
                     </li>
                 </ul>
@@ -139,20 +176,11 @@ function App() {
                                     <button className="btn btn-primary" onClick={removeAccount}>Disconnect Wallet</button>
                                 )
                             }
-
-                            <ul className="navbar-nav ml-auto">
-                                <li className="nav-item dropdown no-arrow">
-                                    <a className="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        <span className="mr-2 d-none d-lg-inline text-gray-600 small">{currentAccount || "No account connected"}</span>
-                                    </a>
-                                </li>
-                            </ul>
                         </nav>
 
                         <div className="container-fluid">
                             <div className="d-sm-flex align-items-center justify-content-start mb-4">
-                                <h2 className="h2 mb-0 text-gray-800 mr-3">Dashboard</h2>
-                                <button className="btn btn-outline-secondary" onClick={getCrypto}><i className="fas fa-sync-alt"></i></button>
+                                <h2 className="h2 mb-0 text-gray-800 mr-3">My account: {currentAccount || "No account connected"}</h2>
                             </div>
 
                             <div className="row">
@@ -161,9 +189,9 @@ function App() {
                                         <div className="card-body">
                                             <div className="row no-gutters align-items-center">
                                                 <div className="col mr-2">
-                                                    <div className="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                                    <div className="text-sm font-weight-bold text-primary text-uppercase mb-1">
                                                         Balance</div>
-                                                    <div className="h5 mb-0 font-weight-bold text-gray-800">{currentBalance || "Not connected"}</div>
+                                                    <div className="h5 mb-0 font-weight-bold text-gray-800">{currentBalance ? parseNumber(currentBalance) + " ETH" : "Not connected"}</div>
                                                 </div>
                                             </div>
                                         </div>
@@ -173,6 +201,7 @@ function App() {
 
                             <div className="d-sm-flex align-items-center justify-content-start mb-4">
                                 <h3 className="h4 mb-0 text-gray-800 mr-3">Latest Listings</h3>
+                                <button className="btn btn-outline-secondary" onClick={getCrypto}><i className="fas fa-sync-alt"></i></button>
                             </div>
 
                             <div className="row">
@@ -184,13 +213,13 @@ function App() {
                                                     <div className="card-body">
                                                         <div className="row no-gutters align-items-center">
                                                             <div className="col mr-2">
-                                                                <div className="text-xs font-weight-bold text-gray-800 text-uppercase mb-1">
-                                                                    {v?.name || `Crypto ${k + 1}`}</div>
-                                                                <div className="h5 mb-0 font-weight-bold text-gray-800">{parseFloat(v?.quote?.USD?.price).toFixed(3) || "0"} USD</div>
-                                                                <div className="text-xs mb-0 text-gray-800">({parseFloat(v?.quote?.USD?.percent_change_24h).toFixed(3) || "0"}%)</div>
+                                                                <div className="text-sm font-weight-bold text-gray-800 text-uppercase mb-1">
+                                                                    {v?.name || `Cryptocurrency ${k + 1}`} ({v?.symbol})</div>
+                                                                <div className={"h5 mb-0 font-weight-bold text-gray-800"}>{parseNumber(v?.quote?.USD?.price) || "0"} USD</div>
+                                                                <div className={`text-xs mb-0 ${v?.quote?.USD?.percent_change_24h <= 0 ? "text-danger" : "text-success"}`}>({parseNumber(v?.quote?.USD?.percent_change_24h) || "0"}%)</div>
                                                             </div>
                                                             <div className="col-auto">
-                                                                <img src={`./assets/img/${v?.slug}.png`} style={{ width: '50px' }} />
+                                                                <img src={images[k]} style={{ width: '50px' }} alt="" />
                                                             </div>
                                                         </div>
                                                     </div>
